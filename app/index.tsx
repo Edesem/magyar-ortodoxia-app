bcc3ef1d9123f491c313d5f54c5df4aac4e596d1
Mashed <leventezoltan@protonmail.com>
2026-02-22 07:26:59 +1100

    feat: add select and display prayer

--- app/index.tsx
+++ app/index.tsx
@@ -1,20 +1,36 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, ScrollView, StyleSheet, Text, View } from "react-native";
 import { Prayer, prayerData } from "../data/prayers";
+import React, { useState } from "react";
+import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
 
 const prayers: Prayer[] = prayerData;
 
 export default function Index() {
+  const [selectedPrayerId, setSelectedPrayerId] = useState<number | null>(null);
+
   return (
-    <View style={styles.container}>
-      {prayers.map((prayer, index) => (
-        <>
-          <Text>{prayer.title}</Text>
-          <Text>{prayer.text}</Text>
-        </>
-      ))}
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container} edges={['top']}>
+        <ScrollView>
+          {prayers.map((prayer, index) => (
+            <View style={{ marginBottom: 50 }} key={prayer.id}>
+              <Pressable
+                onPress={() => {
+                  selectedPrayerId === null
+                    ? setSelectedPrayerId(prayer.id)
+                    : setSelectedPrayerId(null);
+                }}
+              >
+                <Text style={{fontWeight: "bold"}}>{prayer.title}</Text>
+                {selectedPrayerId == prayer.id && <Text>{prayer.text}</Text>}
+              </Pressable>
+            </View>
+          ))}
+          <StatusBar style="auto" />
+        </ScrollView>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
 
@@ -24,5 +40,7 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    paddingTop: 40,
+    paddingHorizontal: 40,
   },
 });
--- data/prayers.ts
+++ data/prayers.ts
@@ -9,10 +9,35 @@ export const prayerData: Prayer[] = [
         id: 0,
         title: "Reggelli Imák",
         text: `
-Lord Jesus Christ, Son of God,
-have mercy on me, a sinner.
+Szent Atyáink imái által, Urunk Jézus Krisztus, Istenünk, irgalmazz nékünk.
 
-Amin.
+Dicsőség Néked, Istenünk, dicsőség Néked.
+
+Mennyei Király, Vigasztaló, igazságnak Lelke, Aki mindenütt jelen vagy és mindeneket betöltesz, minden javak kincsestára és az élet adományozója, jöjj és lakozzál mibennünk, és tisztíts meg minket minden szennyfolttól, és üdvözítsd, Jóságos, a mi lelkünket.
+
+Szent Isten, Szent Hatalmas, Szent Halhatatlan, irgalmazz nekünk. (3-szor)
+
+Dicsőség az Atyának és Fiúnak és Szent Léleknek, most és mindenkor és mindörökkön örökké. Ámin.
+
+Szentséges Háromság, irgalmazz nekünk. Urunk, könyörülj a mi bűneinken. Uralkodónk, bocsásd meg törvényszegéseinket. Szent keresd fel és gyógyítsd meg a mi betegségeinket, a Te Nevedért.
+
+Uram irgalmazz. (3-szor)
+
+Dicsőség az Atyának és Fiúnak és Szent Léleknek, most és mindenkor és mindörökkön örökké. Ámin.
+
+Mi Atyánk, Ki a mennyekben vagy, szenteltessék meg a Te Neved, jöjjön el a Te Országod, legyen meg a Te akaratod, miképpen a mennyben, úgy a földön is. Mindennapi kenyerünket add meg nekünk ma; és bocsásd meg a mi vétkeinket, miképpen mi is megbocsátunk az ellenünk vétkezőknek, és ne vígy minket kísértésbe, hanem szabadíts meg a gonosztól.
+
+Álmunkból felkelvén, leborulunk Előtted, Jóságos, és az angyalok dicsérőénekét zengjük Néked, óh Hatalmas: Szent, Szent, Szent vagy Isten, irgalmazz nekünk Isten Szülője által.
+
+Dicsőség az Atyának és Fiúnak és Szent Léleknek…
+
+Felkeltvén engem ágyamról és álmomból, Uram, világosítsd meg elmémet és nyisd meg szívemet és ajkaimat a Te dicséretedre, Szent Háromság: Szent, Szent, Szent vagy Isten, irgalmazz nekünk Isten Szülője által.
+
+Most és mindenkor és mindörökkön örökké. Ámin.
+
+Váratlanul jő el a Bíró, és mindenkinek cselekedetei lelepleztetnek, kiáltsuk hát félelemmel az éj közepén: Szent, Szent, Szent vagy Isten, irgalmazz nekünk Isten Szülője által.
+
+Uram irgalmazz. (12-szer)
         `
     },
     {

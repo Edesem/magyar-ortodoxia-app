4ed2ed03860b86f0198f9b39372b2dcaf1494722
Mashed <leventezoltan@protonmail.com>
2026-03-04 21:21:04 +1100

    feat: komboszkini clipart rotation

--- app/(tabs)/komboszkini.tsx
+++ app/(tabs)/komboszkini.tsx
@@ -4,6 +4,8 @@ import {
   StyleSheet,
   Text,
   View,
+  Image,
+  Vibration,
 } from "react-native";
 import React, { useEffect, useState } from "react";
 import { SafeAreaView, SafeAreaProvider } from "react-native-safe-area-context";
@@ -12,24 +14,66 @@ import { COLOURS } from "../../constants/colours";
 
 export default function Index() {
   const [count, setCount] = useState(0);
+  const [rotation, setRotation] = useState(0);
 
   const prayerRope = (event: GestureResponderEvent): void => {
     setCount(count + 1);
     Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+    if (rotation >= 360) {
+      setRotation(0);
+    }
+    setRotation((prevRotation) => prevRotation - 7.2);
+
+    if (count >= 50) {
+      Vibration.vibrate();
+      setCount(0);
+    }
   };
 
   return (
     <SafeAreaProvider>
       <SafeAreaView style={styles.container}>
-        <View style={{ alignItems: "center", position: "absolute", paddingTop: 80, left: 36 }}>
-          <Text style={{color: "grey", fontFamily: "Alegreya_400Regular_Italic", fontSize: 20, textAlign: "center"}}>„Digitális komboszkini – ez egy kiegészítő, nem helyettesíti a hagyományos komboszkinít.”</Text>
+        <View
+          style={{
+            alignItems: "center",
+            position: "absolute",
+            paddingTop: 80,
+            left: 36,
+          }}
+        >
+          <Text
+            style={{
+              color: "grey",
+              fontFamily: "Alegreya_400Regular_Italic",
+              fontSize: 20,
+              textAlign: "center",
+            }}
+          >
+            „Digitális komboszkini – ez egy kiegészítő, nem helyettesíti a
+            hagyományos komboszkinít.”
+          </Text>
         </View>
         <Pressable
           onPress={prayerRope}
           style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
         >
-          <Text style={{ fontSize: 100, fontFamily: "AlegreyaSC_400Regular", color: COLOURS.red }}>
-            {count}
+          <Image
+            source={require("../../assets/50komboszkini.png")}
+            style={[
+              styles.komboszkini,
+              { transform: [{ rotate: `${rotation}deg` }] },
+            ]}
+          />
+
+          <Text
+            style={{
+              fontSize: 50,
+              fontFamily: "AlegreyaSC_400Regular",
+              color: COLOURS.red,
+              textAlign: "center",
+            }}
+          >
+            50 csomó
           </Text>
         </Pressable>
       </SafeAreaView>
@@ -44,5 +88,10 @@ const styles = StyleSheet.create({
     paddingTop: 40,
     paddingHorizontal: 40,
     color: "#000",
+  },
+  komboszkini: {
+    width: 400,
+    height: 400,
+    tintColor: COLOURS.deep_red,
   },
 });

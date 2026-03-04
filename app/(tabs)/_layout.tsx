d9b1e909eb8ac3f6466f97033165720f945511ad
Mashed <leventezoltan@protonmail.com>
2026-03-04 21:47:55 +1100

    add: seperate tab layout for ios and android

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,26 +1,57 @@
 import React from "react";
 import { Icon, Label, NativeTabs } from "expo-router/unstable-native-tabs";
-import { DynamicColorIOS, Platform } from "react-native";
+import { DynamicColorIOS, Platform, Image } from "react-native";
 import { COLOURS } from "../../constants/colours";
+import { Tabs } from "expo-router";
 
-export default function TabsLayout() {
-  const isIOS = Platform.OS === "ios";
-  
+function IOSTabs() {
   return (
-    <NativeTabs tintColor={COLOURS.deep_red} >
+    <NativeTabs tintColor={COLOURS.deep_red}>
       <NativeTabs.Trigger name="index">
         <Label>Imák</Label>
-        <Icon
-  sf={isIOS ? "book.fill" : undefined}
-  src={!isIOS ? require("../../assets/icons/book.png") : undefined} />
+        <Icon sf={"book.fill"} />
       </NativeTabs.Trigger>
       <NativeTabs.Trigger name="komboszkini">
         <Label>Komboszkini</Label>
-        <Icon
-          sf={isIOS ? "circle.grid.cross.fill" : undefined}
-          src={!isIOS ? require("../../assets/icons/grid.png") : undefined} />
-        />
+        <Icon sf={"circle.grid.cross.fill"} />
       </NativeTabs.Trigger>
     </NativeTabs>
   );
+}
+
+function AndroidTabs() {
+  return (
+    <Tabs screenOptions={{ tabBarActiveTintColor: "blue" }}>
+      <Tabs.Screen
+        name="index"
+        options={{
+          title: "Imák",
+          tabBarIcon: ({ color }) => (
+            <Image
+              source={require("../../assets/icons/book.png")}
+              resizeMode="contain"
+              style={{ width: 25, height: 25}}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="komboszkini"
+        options={{
+          title: "Komboszkini",
+          tabBarIcon: ({ color }) => (
+            <Image
+              source={require("../../assets/icons/grid.png")}
+              resizeMode="contain"
+              style={{ width: 25, height: 25}}
+            />
+          ),
+        }}
+      />
+    </Tabs>
+  );
+}
+
+export default function TabsLayout() {
+  return Platform.OS === "ios" ? <IOSTabs /> : <AndroidTabs />;
 }

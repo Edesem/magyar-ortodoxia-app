021c054c11b6cc3c8bcd95eed1c49a90eac293e7
Mashed <leventezoltan@protonmail.com>
2026-03-18 20:48:45 +1100

    style: add bottom image to index

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -114,6 +114,17 @@ export default function Index() {
         </View>
       ))}
       <StatusBar style="auto" />
+
+      <Image
+        source={require("../../assets/orthodox/TwoBarCross.png")}
+        style={{
+          width: "100%",
+          height: 300,
+          resizeMode: "contain",
+          marginVertical: 50,
+          tintColor: theme.header,
+        }}
+      />
     </ScrollView>
   );
 }

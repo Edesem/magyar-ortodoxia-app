324a14a5307d04593e47dcac072652f9586c60e0
Mashed <leventezoltan@protonmail.com>
2026-03-26 22:37:43 +1100

    style: fix formatting for HT akathist

--- data/prayers/akathists/holy_trinity_akathist.ts
+++ data/prayers/akathists/holy_trinity_akathist.ts
@@ -2,7 +2,7 @@ export const holy_trinity_akathist = {
   id: "holy_trinity_akathist",
   title: `Az egylényegű, mindenható és oszthatatlan Háromsághoz`,
   header_title:
-    "A Háromsághoz",
+    "A Szentháromsághoz",
   image: "",
   sections: [
     {
@@ -15,17 +15,29 @@ export const holy_trinity_akathist = {
       text: `Kezdet nélküli Istenfejedelemség, Atya, Ige és Lélek, Háromság, egyedüli Istenként egységes egylényegű, de három formára elkülönült természet; a teremtés, amelyet puszta Szavad hozott létre, így kiált hozzád, Teremtőjéhez:
 
 Dicsőség néked, háromszemélyű Isten,
+
 Dicsőség néked, egyedüli Oszthatatlan!
+
 Dicsőség néked, Atya, Fiú és Lélek,
+
 Dicsőség néked, egyetlen háromszoros-napú Isten!
+
 Dicsőség néked, aki kezeddel formáltad Ádámot,
+
 Dicsőség néked, aki leheleteddel életre keltetted!
+
 Dicsőség a te mindenben segítő bölcs akaratodnak,
+
 Dicsőség mindenestül kifejezhetetlen isteni alászállásodnak!
+
 Dicsőség néked, mindenek kimondhatatlan élete,
+
 Dicsőség néked, élők isteni lehelete!
+
 Dicsőség elbeszélhetetlen bölcsességednek,
+
 Dicsőség mindenekre kiterjedő gondoskodásodnak!
+
 Dicsőség néked, Istenünk!`,
     },
     {
@@ -35,17 +47,29 @@ Dicsőség néked, Istenünk!`,
 Imádva a Fiú születését és a Lélek kiáradását a kezdet nélküli Atyától, az Istent, aki egy lényegben van, de alakjában három személyű, a te dicsőségednek mutatunk be isteni szolgálatot így kiáltva:
 
 Dicsőség az Atyának, aki nem született,
+
 Dicsőség a Fiúnak, aki megszületett!
+
 Dicsőség a kiárasztott szentséges Léleknek,
+
 Dicsőség a Háromságban egy Istennek!
+
 Dicsőség néked, mindent-tehető Atya, mindenek Teremtője!
+
 Dicsőség néked, életetadó Ige, emberek Megváltója!
+
 Dicsőség néked, Isteni Lélek, aki betöltöd az egész teremtést,
+
 Dicsőség néked, mindenek Istene, lényegfölötti Természet!
+
 Dicsőség néked, mert te vagy a mindenek Oka,
+
 Dicsőség néked, mert erőd mindent felülmúl!
+
 Dicsőség néked, Háromság, egylényegű Hatalom,
+
 Dicsőség néked, Egység, háromszemélyű Szépség!
+
 Dicsőség néked, Istenünk!`,
     },
     {
@@ -54,17 +78,29 @@ Dicsőség néked, Istenünk!`,
  Szóltál: „Legyen világosság!” – és az angyali rendek azonnal megszülettek, hogy ott álljanak rettegéssel a te félelmetes trónod előtt, dicsőítve megközelíthetetlen dicsőséged hatalmát:
 
 Dicsőség néked, az egész föld Alapjának,
+
 Dicsőség a menny Pólusának!
+
 Dicsőség néked, angyalok isteni Fensége,
+
 Dicsőség, arkangyalok kifejezhetetlen Tisztessége!
+
 Dicsőség néked, Fény, aki túlragyogsz minden fényességes fejedelemséget,
+
 Dicsőség, Ragyogás, megközelíthetetlen minden hatalmasságnak!
+
 Dicsőség néked, az erők végtelen Hatalmának,
+
 Dicsőség az uralmak káprázatos Szépségének!
+
 Dicsőség néked, trónusok páratlan Méltósága,
+
 Dicsőség, anyagtalan természetek Ruhája!
+
 Dicsőség néked, kerubok éneket zengő Hangja,
+
 Dicsőség, szeráfok háromszorszent Himnusza!
+
 Dicsőség néked, Istenünk!`,
     },
     {
@@ -74,17 +110,29 @@ Dicsőség néked, Istenünk!`,
 Bevilágítottad a világmindenséget, amikor felerősítetted a nagy lámpásokat a menny magasára, hogy a nagyobbik uralkodjék örökké a nappal fölött, a kisebbik pedig a csillagokkal együtt az éjszaka fölött. Ezért elgondolva háromszoros napod világosságát így kiáltsunk:
 
 Dicsőség háromszoros napod világosságának,
+
 Dicsőség a háromsugarú fényárnak!
+
 Dicsőség szépséged felragyogásának;
+
 Dicsőség az egyedüli istenség kisugárzásának!
+
 Dicsőség néked, egy Istenség aranyos csillámlása,
+
 Dicsőség, angyali formák beragyogása!
+
 Dicsőség néked, aki fényárba öltözteted a Napot,
+
 Dicsőség néked, aki ragyogóvá teszed a Holdat és minden csillagot!
+
 Dicsőség végtelen fényességednek,
+
 Dicsőség anyagtalan tündöklésednek!
+
 Dicsőség néked, aki világosságot derít a mindenségre,
+
 Dicsőség néked, aki fénybe borítja a világot!
+
 Dicsőség néked, Istenünk!`,
     },
     {
@@ -94,17 +142,29 @@ Dicsőség néked, Istenünk!`,
 Hogy erőd túláradó bősége megnyilvánuljon, megparancsoltad a vizeknek, hogy hozzák elő a cetek és halak sokaságát, a földön pedig szárnyas madarakat, hogy gyönyörűségükre szolgáljanak az embereknek, akik így kiáltanak:
 
 Dicsőség mondhatatlan gondviselésednek,
+
 Dicsőség végtelen jóságodnak!
+
 Dicsőség néked, aki fákkal ékesítetted fel a földet,
+
 Dicsőség néked, aki táplálékot készítettél minden élőnek!
+
 Dicsőség néked, aki a hegyekből forrásokat fakasztottál,
+
 Dicsőség néked, aki a vizekből madarakat röptettél!
+
 Dicsőség néked, aki a tengerben megalkottad az állatokat,
+
 Dicsőség néked, aki halakat adtál a halandóknak!
+
 Dicsőség a te isteni akaratodnak,
+
 Dicsőség kimondhatatlan parancsodnak!
+
 Dicsőség legyőzhetetlen hatalmadnak,
+
 Dicsőség elképzelhetetlen erődnek!
+
 Dicsőség néked, Istenünk!`,
     },
     {
@@ -114,17 +174,29 @@ Dicsőség néked, Istenünk!`,
 Mennydörgéssel leküldöd villámló ragyogásodat az esőt szülő felhőkből, és az évszakok szerint teljes összhangban kevered a szeleket, hogy alkalmas eledelt érleljenek mindazoknak, akik így kiáltanak hozzád:
 
 Dicsőség néked, aki az éterben ragyogsz,
+
 Dicsőség néked, aki a levegőben villámlasz!
+
 Dicsőség néked, aki esőt árasztasz a földre,
+
 Dicsőség néked, aki sokféle szelet fújsz!
+
 Dicsőség hatalmad mennydörgő erejének,
+
 Dicsőség magokat sarjasztó mondhatatlan mélységednek!
+
 Dicsőség néked, aki évszakonként megadod a kellő szelet,
+
 Dicsőség néked, aki minden időben adsz friss eledelt!
+
 Dicsőség néked, aki leheletet ajándékozol minden élőnek,
+
 Dicsőség néked, aki életet adsz mindeneknek!
+
 Dicsőség isteni munkálkodásodnak,
+
 Dicsőség mindent beteljesítő gondviselésednek!
+
 Dicsőség néked, Istenünk!`,
     },
     {
@@ -134,17 +206,29 @@ Dicsőség néked, Istenünk!`,
 Isteni hatású intéseddel a földből teremtettél minden állatot, a csúszómászókat és a vadállatokat, a négylábúakat és az összes madarat nemük szerint, hogy betöltsék az egész szárazföldet, és szolgálják a híveket, akik így kiáltanak hozzád:
 
 Dicsőség a mindenek Teremtőjének,
+
 Dicsőség a mindeneket Életrekeltőnek!
+
 Dicsőség a mindent rendben Létrehozónak,
+
 Dicsőség néked, mert állatokat adtál a halandóknak!
+
 Dicsőség az állatokat földből Előhívónak,
+
 Dicsőség a földet élőlényekkel Betöltőnek!
+
 Dicsőség néked, aki a barmokat szolgálatunkra rendelted,
+
 Dicsőség néked, mert a vadállatokat félelemmel töltötted!
+
 Dicsőség mindent-okozó akaratodnak,
+
 Dicsőség felfoghatatlan tanácsodnak!
+
 Dicsőség világteremtő alkotásodnak,
+
 Dicsőség mindent véghezvivő akaratodnak!
+
 Dicsőség néked, Istenünk!`,
     },
     {
@@ -154,17 +238,29 @@ Dicsőség néked, Istenünk!`,
 Hogy megmutasd hármas ragyogású istenségedet, így szóltál: Teremtsünk embert hozzánk hasonló képmásra! Ki mint Isten, lelket leheltél a földdarabba, értelemmel, ésszel és szellemmel tökéletesíted az embert, hogy így szóljon:
 
 Dicsőség a mindenek egyetlen Istenének,
+
 Dicsőség az egyedüli Felfoghatatlannak!
+
 Dicsőség néked, Háromság az istenség Egyetlenségében,
+
 Dicsőség néked, Egyetlenség a három személyben!
+
 Dicsőség lényegiséged felfoghatatlan Alakjának,
+
 Dicsőség a több mint kimondhatatlan isteni Megvilágosításnak!
+
 Dicsőség a több mint kezdetnélküli, nemszületett Szülőnek,
+
 Dicsőség az Atyától vele együtt kezdet nélkül Szülöttnek,
+
 Dicsőség az Atyától eredő isteni Személynek!
+
 Dicsőség annak, akitől az ember lett,
+
 Dicsőség annak, aki által a halandó természet alakot nyert,
+
 Dicsőség annak, akiben az fennmarad!
+
 Dicsőség néked, Istenünk!`,
     },
     {
@@ -174,17 +270,29 @@ Dicsőség néked, Istenünk!`,
 Nincs nyelv, legyen bármilyen ékesszóló, amely képes lenne alkotásaid dicséretére, amelyeket hat nap alatt alkottál, majd a hetedik napon megpihentél minden munkádtól, és megáldottad és megszentelted azt a napot. Ezért így kiáltunk hozzád:
 
 Dicsőség mindenek Megalkotójának,
+
 Dicsőség a mindenség Beteljesítőjének!
+
 Dicsőség néked, mert gondviseléseddel megalapoztad a teremtést,
+
 Dicsőség néked, mert erőddel bevégezted a világot!
+
 Dicsőség kimondhatatlan Igédnek, aki által a világ látható lett,
+
 Dicsőség isteni végzésednek, amely által minden nagyon jó lett!
+
 Dicsőség néked, aki hat nap alatt bevégeztél mindent,
+
 Dicsőség néked, aki a hetedik napon mindentől megpihent!
+
 Dicsőség néked, akitől minden eredt,
+
 Dicsőség néked, aki által minden megszületett,
+
 Dicsőség néked, akiben minden beteljesedett,
+
 Dicsőség néked, Isten, aki uralkodsz mindenek felett!
+
 Dicsőség néked, Istenünk!`,
     },
     {
@@ -194,17 +302,29 @@ Dicsőség néked, Istenünk!`,
 Az oktalan állatok természetét odavezetted ahhoz, akit te okosítottál fel, hogy lásd, minek nevezi őket, és aminek hívta őket, az maradt a nevük, s rajtuk uralkodván így kiált hozzád:
 
 Dicsőség a megközelíthetetlen Úrnak,
+
 Dicsőség az egyedüli Mindenhatónak!
+
 Dicsőség néked, aki a szóba tudást árasztasz,
+
 Dicsőség néked, aki a lehelettel a szónak jelentést adsz!
+
 Dicsőség néked, aki az oktalan állatokat alávetetted,
+
 Dicsőség néked, aki az okos természetet felemelted!
+
 Dicsőség néked, aki a Paradicsomot az embernek juttattad,
+
 Dicsőség néked, aki a földet minden élőlénynek adtad!
+
 Dicsőség néked, aki kezeddel forgatod a mindenség körforgását,
+
 Dicsőség a te végtelen jóságodnak!
+
 Dicsőség néked, a mindenség Istenfejedelmének,
+
 Dicsőség a te isteni uralmadnak!
+
 Dicsőség néked, Istenünk!`,
     },
     {
@@ -214,15 +334,25 @@ Dicsőség néked, Istenünk!`,
 A bosszúálló démon nem tudta elviselni a halandókra fényt derítő dicsőséget, amelyet a halandó természetnek adtál, ezért magára öltötte a ravasz kígyó alakját, s az asszonyra lövellte az önistenítés mérgét, az pedig átadta a férfinak, aki megfeledkezett róla, hogy téged dicsőítsen így szólva:
 
 Dicsőség az egyetlen Istennek, aki van,
+
 Dicsőség néked, az egyedüli Halhatatlannak!
+
 Dicsőség néked, aki a nemlétből teremtetted a mindenséget,
+
 Dicsőség néked, aki kezeddel formáltad az embert!
+
 Dicsőség néked, aki jóságodban a Paradicsomot adtad lakhelyéül,
+
 Dicsőség néked, aki arra méltattad, hogy uralkodjék az egész földön,
+
 Dicsőség néked, aki megadtad, hogy ehessék minden fáról!
+
 Dicsőség néked, Fiú, aki fenntartod a világot,
+
 Dicsőség néked, Lehelet, akiben minden fennmarad,
+
 Dicsőség néked, egyetlen istenfejedelmi Háromság!
+
 Dicsőség néked, Istenünk!`,
     },
     {
@@ -232,17 +362,29 @@ Dicsőség néked, Istenünk!`,
 Azáltal, hogy alászállt és testet vett a tiszta Szűztől, kinyilatkoztattad, ó, egyedüli Háromság a lélekmentő Igét, aki az Atyától született, és örökké együtt van a Lélekkel. Ezért nevezte Ádám Életnek Évát, így kiáltva hozzád:
 
 Dicsőség néked, ó, emberszerető Atyám,
+
 Dicsőség néked, jóságos és irgalmas Istenem!
+
 Dicsőség néked, ó, Fiú, aki újjáalkottál engem,
+
 Dicsőség néked, aki arra készülsz, hogy a tiszta Szűztől születve megments engem!
+
 Dicsőség néked is Szentlélek, aki beárnyékoltad a Tisztaságost,
+
 Dicsőség néked, aki beteljesítetted a félelmetes misztériumot!
+
 Dicsőség a kimondhatatlan és isteni leereszkedésnek,
+
 Dicsőség az emberi lényeg üdvösségének!
+
 Dicsőség néked, akitől élet fakad számomra,
+
 Dicsőség néked, akitől lelkem üdvösséget kap!
+
 Dicsőség néked, akiben újraformálódom,
+
 Dicsőség néked, aki által megújul a teremtés!
+
 Dicsőség néked, Istenünk!`,
     },
     {

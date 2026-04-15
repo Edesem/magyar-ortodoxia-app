63a41e38c74f37025b854f1d2b772b7bcc1a62ff
Mashed <leventezoltan@protonmail.com>
2026-04-15 19:30:53 +1000

    chore: delete console log for orientation

--- hooks/useOrientation.tsx
+++ hooks/useOrientation.tsx
@@ -12,7 +12,6 @@ const getOrientation = () => {
     useEffect(() => {
         Dimensions.addEventListener("change", () => {
           setOrientation(getOrientation());
-          console.log(orientation);
         });
       });
 
